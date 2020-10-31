d234b349e715d27b6a4b7fb98c888f664d42d619
errolcostabir <errolcostabir98@gmail.com>
2020-10-31 15:52:34 +0530

    event section complete

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-const String url = 'http://ip/CleanGoa/getData.php';
+import 'package:firebase_database/firebase_database.dart';
 
 class FirstPage extends StatefulWidget {
   const FirstPage({Key key}) : super(key: key);
@@ -12,91 +11,135 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
-  Future<List> getData() async {
-    final responce = await http.get(url);
-    return jsonDecode(responce.body);
+  List<Posts> postsList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    DatabaseReference postsRef =
+        FirebaseDatabase.instance.reference().child("Posts");
+    postsRef.once().then((DataSnapshot snap) {
+      var KEYS = snap.value.keys;
+      var DATA = snap.value;
+
+      postsList.clear();
+
+      for (var individualKey in KEYS) {
+        Posts posts = new Posts(
+            DATA[individualKey]['image'], DATA[individualKey]['description'],DATA[individualKey]['address']);
+        postsList.add(posts);
+      }
+
+      setState(() {
+        print('Length: $postsList.length');
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF111328),
-      body: FutureBuilder<List>(
-        future: getData(),
-        builder: (context, ss) {
-          if (ss.hasError) {
-            print(ss.error);
-          }
-          if (ss.hasData) {
-            return Docs(
-              list: ss.data,
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
+        backgroundColor:Color(0xFFBBDEFB), 
+        body:
+            Container(
+                child: postsList.length == 0
+                    ? new Text("No events")
+                    : new ListView.builder(
+                        itemCount: postsList.length,
+                        itemBuilder: (_, index) {
+                          return Doc(postsList[index].image,
+                              postsList[index].description,postsList[index].address);
+                        })));
   }
 }
 
-class Docs extends StatelessWidget {
-  Docs({this.list});
-  final List list;
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      
-      itemCount: list == null ? 0 : list.length,
-      itemBuilder: (context, i) {
-        return Container(
-        //  color: Colors.black54,
-          child: Card(
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-             // elevation: 6.0,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      child: Image.network(list[i]['photo']),
-                      height: 200.0,
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(list[i]['description'],style: TextStyle(
-                      color: Colors.black54,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: 'Montserrat' 
-                    ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        SizedBox(
-                          
-                          height: 20.0,
-                          child: Icon(Icons.favorite_border,color: Colors.redAccent,),
-                        ),
-                      ],
-                    )
-                  ],
+
+class Posts {
+  String image, description,address;
+  Posts(this.image, this.description,this.address);
+}
+
+addEvent(){
+
+}
+
+Widget Doc(String image, String description,String address) {
+  return Card(
+    margin: EdgeInsets.all(8.0),
+    elevation: 20.0,
+      color: Colors.white70,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 20.0,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                Icons.location_on,
+                color: Colors.black,
                 ),
-                
+                Text(
+                  address,
+                  style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Montserrat'),
+                  ),
+                ],
               )
+            ),
+            SizedBox(
+              height: 5.0,
+
+            ),
+            Container(
+              child: Image.network(image),
+              height: 200.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+                description,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Montserrat'),
               ),
-         // margin: EdgeInsets.all(5.0),
-        );
-      },
-    );
-  }
+              SizedBox(
+                height: 5.0,
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 20.0,
+                  child: Icon(
+                    Icons.favorite_border,
+                    color: Colors.redAccent,
+                  ),
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                SizedBox(
+                  height: 20.0,
+                  
+                  child: RaisedButton(
+                  child: Text('Attend Drive'),
+                  onPressed: addEvent(),
+                  color: Color(0xFF007ACC),
+                  textColor: Colors.white,
+                ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ));
 }
